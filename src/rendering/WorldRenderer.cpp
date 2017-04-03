091d7eed6979eaa24f20226cf7b957920eba1877
Krzysztof Antoniak <krzysztof@antoniak.in>
2017-04-03 14:35:54 +0200

    Fix image deallocation

--- src/rendering/WorldRenderer.cpp
+++ src/rendering/WorldRenderer.cpp
@@ -79,7 +79,7 @@ bool WorldRenderer::setupTextures() {
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
     glGenerateMipmap(GL_TEXTURE_2D);
-    delete[] pixels;
+    stbi_image_free(pixels);
   }
 
   {
@@ -95,7 +95,7 @@ bool WorldRenderer::setupTextures() {
     glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    delete[] pixels;
+    stbi_image_free(pixels);
   }
 
   return true;
