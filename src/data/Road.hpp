5a8c72aa35f196e28707b5c5514c892fab807a3a
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-08-24 18:09:54 -0700

    Add diving roads between chunks

---
+++ src/data/Road.hpp
@@ -0,0 +1,21 @@
+#ifndef DATA_ROAD_HPP
+#define DATA_ROAD_HPP
+
+#include <vector>
+
+#include "../data/Position.hpp"
+
+namespace data {
+
+class Road {
+
+public:
+  Road(std::vector<Position> tiles);
+  const std::vector<Position> getTiles() const;
+
+private:
+  std::vector<Position> tiles;
+};
+}
+
+#endif
--- src/input/LineSelection.cpp
+++ src/input/LineSelection.cpp
@@ -46,4 +46,59 @@ const std::vector<data::Position> LineSelection::getSelected() const {
 
   return result;
 }
+
+const std::vector<LineSelection> LineSelection::divideByChunk() const {
+  std::vector<LineSelection> result;
+
+  const data::Position from(this->getFrom());
+  const data::Position to(this->getTo());
+
+  if (from.getChunk().x != to.getChunk().x) {
+    glm::ivec2 sameChunkEnd = to.getGlobal();
+    sameChunkEnd.x = from.getGlobal().x - from.getLocal().x + (data::Chunk::SIDE_LENGTH - 1);
+
+    LineSelection sameChunk(lineWidth);
+    sameChunk.from(from.getGlobal());
+    sameChunk.to(sameChunkEnd);
+    result.push_back(sameChunk);
+
+    glm::ivec2 nextChunkStart = sameChunkEnd;
+    nextChunkStart.x += 1;
+
+    LineSelection remainder(lineWidth);
+    remainder.from(nextChunkStart);
+    remainder.to(to.getGlobal());
+    result.push_back(remainder);
+  } else if (from.getChunk().y != to.getChunk().y) {
+    glm::ivec2 sameChunkEnd = to.getGlobal();
+    sameChunkEnd.y = from.getGlobal().y - from.getLocal().y + (data::Chunk::SIDE_LENGTH - 1);
+
+    LineSelection sameChunk(lineWidth);
+    sameChunk.from(from.getGlobal());
+    sameChunk.to(sameChunkEnd);
+    result.push_back(sameChunk);
+
+    glm::ivec2 nextChunkStart = sameChunkEnd;
+    nextChunkStart.y += 1;
+
+    LineSelection remainder(lineWidth);
+    remainder.from(nextChunkStart);
+    remainder.to(to.getGlobal());
+    result.push_back(remainder);
+  } else {
+    LineSelection same(lineWidth);
+    same.from(from.getGlobal());
+    same.to(to.getGlobal());
+    result.push_back(same);
+  }
+
+  // TODO(kantoniak): Handle case when wide line ends on the other chunk
+  return result;
+}
+
+unsigned int LineSelection::getLength() const {
+  const glm::ivec2 diff = toPoint - fromPoint;
+  const bool goesNorth = std::abs(diff.y) > std::abs(diff.x);
+  return 1 + (goesNorth ? std::abs(diff.y) : std::abs(diff.x));
+}
 }
