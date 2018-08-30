e56a2e86b0e16f5d53e5b2427ab025ffec88f5d0
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-08-29 20:21:45 -0700

    Prevent from adding roads which collide with buildings

--- src/world/Geometry.hpp
+++ src/world/Geometry.hpp
@@ -29,6 +29,7 @@ public:
 
   // TODO(kantoniak): Return structure with object IDs by type
   bool checkCollisions(const data::buildings::Building& building) const;
+  bool checkCollisions(const data::Road& road) const;
 
   std::vector<data::buildings::Building> getBuildings(const glm::ivec2 from, const glm::ivec2 to) const;
 
