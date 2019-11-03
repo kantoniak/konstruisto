734ac5619b6e54efd28b45ddb97d48d50f1c409f
Krzysztof Antoniak <krzysztof@antoniak.in>
2019-11-03 16:42:56 +0100

    Create Powershell script to set up dependencies

--- README.md
+++ README.md
@@ -17,72 +17,45 @@ Only x64 builds are supported. MSVC is preferred for Windows. Windows GNU versio
 
 ### Building on Linux
 
-1. Set up dependencies (below)
-1. Run `make rebuild run`
-
-To build release configuration: `make rebuild run CONFIG=RELEASE`. Command `make help` shows all possible options.
-
-#### Linux dependencies
-
-You can just run `setup-deps.sh`, it will setup everything for you.
-
+1. Set up dependencies running `scripts/setup-deps.sh` from the main directory:
+    ````
     git clone git@github.com:kantoniak/konstruisto.git konstruisto &&
     cd konstruisto &&
     chmod +x ./scripts/setup-deps.sh &&
     sudo ./scripts/setup-deps.sh &&
-    cd - &&
-    make build run
+    cd -
+    ````
+1. Run `make build run`
 
-If you want to do everything by hand, here's the list of dependencies:
+To build release configuration use `make rebuild run CONFIG=RELEASE`. Command `make help` shows all possible options.
 
-1. Install `clang-9 clang-format-9 clang-tidy-9 clang-tools-9`.
-1. Install `libglew-dev` (2.1.0): https://launchpad.net/ubuntu/+source/glew
-1. Install `libglfw3-dev` (3.3): https://launchpad.net/ubuntu/+source/glfw3
-1. Download `glm` and extract to `ext/glm-0.9.9.6`
-    ```
-    wget https://github.com/g-truc/glm/releases/download/0.9.9.6/glm-0.9.9.6.zip &&
-    unzip glm-0.9.9.6.zip -d ext/ &&
-    mv "ext/glm" "ext/glm-0.9.9.6" &&
-    rm 0.9.9.6.zip
-    ```
-1. Download `cereal` and extract to `ext/cereal-1.3.0`
-    ```
-    wget https://github.com/USCiLab/cereal/archive/v1.3.0.zip --no-check-certificate -O cereal-1.3.0.zip &&
-    unzip cereal-1.3.0.zip -d ext/ &&
-    rm cereal-1.3.0.zip
-    ```
-1. Download `stb_image.h` to `ext/stb/stb/stb_image.h`
-    ```
-    mkdir -p ext/stb/stb/ &&
-    wget https://raw.githubusercontent.com/nothings/stb/master/stb_image.h -P ext/stb/stb/
-    ```
-1. Build and install `nanovg` (you will need `premake5`)
-    ```
-    git clone git@github.com:memononen/nanovg.git ext/nanovg &&
-    cd ext/nanovg &&
-    premake5 --cc=gcc gmake &&
-    mv build/ build-linux/ &&
-    cd build-linux/ &&
-    make config=release64 nanovg
-    ```
+### Building on Windows
 
-### Windows
+1. Set up dependencies running `scripts/setup-deps.ps1` in Powershell from the main directory:
+    ```
+    git clone git@github.com:kantoniak/konstruisto.git konstruisto
+    Set-Location konstruisto
+    .\scripts\setup-deps.ps1
+    ```
+1. Build NanoVG release libraries:
+    1. Open solution `ext\nanovg\build-msvc\nanovg.sln`
+    1. Build project `nanovg` for Release/x64.
+1. Open Visual Studio solution and build project.
 
-In case of MSVC set up shared dependencies and run build from Visual Studio. For MinGW headers build you will have to put some programs in the `PATH` (see below).
+Please note that only x64 builds are supported.
 
-#### Shared Windows dependencies
+#### (Deprecated) Windows build with MinGW headers
 
+You can use `scripts/setup-deps.ps1` to set up the dependencies.
+
+1. You will need `make`, `clang`, `clang++`, `clang-format` in `PATH`.
+1. You will need `windres` in `PATH` from `mingw-w64`
 1. Install GLEW in `ext/glew-2.1.0`
 1. Install GLFW in `ext/glew-3.3`
 1. Extract `glm` library to `ext/glm-0.9.9.6`
 1. Extract `cereal` library to `ext/cereal-1.3.0`
 1. Put `stb_image.h` to `ext/stb/stb/stb_image.h`
 1. Build and install `nanovg` (see below)
-
-#### GNU-specific dependencies
-
-1. You will need `make`, `clang`, `clang++`, `clang-format` in `PATH`.
-1. You will need `windres` in `PATH` from `mingw-w64`
 
 ### Building `nanovg`
 
@@ -99,7 +72,7 @@ You will need `premake5` [(download)](https://premake.github.io/download.html).
     make config=release64 nanovg
     ```
 
-## <img src="https://opensource.org/files/osi_symbol.png" height="20" alt="Open Source" /> license
+## License
 
 Copyright &copy; 2017-2019 Krzysztof Antoniak
 
---
+++ scripts/setup-deps.ps1
@@ -0,0 +1,153 @@
+# This script sets up Windows Dependencies
+$ErrorActionPreference = "Stop"
+
+$currentDir = Resolve-Path .
+$extDir = Join-Path (Resolve-Path .) "ext\"
+$webClient = New-Object System.Net.WebClient
+
+$glewVersion = "2.1.0"
+$glfwVersion = "3.3"
+$glmVersion = "0.9.9.6"
+$cerealVersion = "1.3.0"
+$premakeVersion = "5.0.0-alpha14"
+
+Write-Output "Setting up Konstruisto dependencies for MSVC..."
+Write-Output "Dependencies directory: $extDir"
+
+# Create directory
+New-Item -ErrorAction Ignore -ItemType directory -Path $extDir | Out-Null
+
+# Step counter
+$step = 0
+$totalSteps = 9
+
+$step++
+# Set up GLEW
+$glewTargetPath = Join-Path $extDir "glew-$glewVersion.zip"
+if (!(Test-Path $glewTargetPath)) {
+    Write-Output "[$step/$totalSteps] Downloading GLEW..."
+    $glewUrl = "https://github.com/nigels-com/glew/releases/download/glew-" + $glewVersion + "/glew-" + $glewVersion + "-win32.zip"
+    $webClient.DownloadFile($glewUrl, $glewTargetPath)
+}
+
+$glewFinalDir = Join-Path $extDir "glew-$glewVersion"
+if (!(Test-Path $glewFinalDir)) {
+    Write-Output "[$step/$totalSteps] Unzipping GLEW..."
+    Expand-Archive $glewTargetPath -DestinationPath $extDir
+}
+
+$step++
+# Set up GLFW
+$glfwTargetPath = Join-Path $extDir "glfw-$glfwVersion.zip"
+if (!(Test-Path $glfwTargetPath)) {
+    Write-Output "[$step/$totalSteps] Downloading GLFW..."
+    $glfwUrl = "https://github.com/glfw/glfw/releases/download/" + $glfwVersion + "/glfw-" + $glfwVersion + ".bin.WIN64.zip"
+    $webClient.DownloadFile($glfwUrl, $glfwTargetPath)
+}
+
+$glfwIntermediateDir = Join-Path $extDir "glfw-$glfwVersion.bin.WIN64"
+$glfwFinalDir = Join-Path $extDir "glfw-$glfwVersion"
+if (!(Test-Path $glfwFinalDir) -And !(Test-Path $glfwIntermediateDir)) {
+    Write-Output "[$step/$totalSteps] Unzipping GLFW..."
+    Expand-Archive $glfwTargetPath -DestinationPath $extDir
+}
+if (Test-Path $glfwIntermediateDir) {
+    Rename-Item -path $glfwIntermediateDir -newName $glfwFinalDir
+}
+
+$step++
+# Set up GLM
+$glmTargetPath = Join-Path $extDir "glm-$glmVersion.zip"
+if (!(Test-Path $glmTargetPath)) {
+    Write-Output "[$step/$totalSteps] Downloading GLM..."
+    $glmUrl = "https://github.com/g-truc/glm/releases/download/$glmVersion/glm-$glmVersion.zip"
+    $webClient.DownloadFile($glmUrl, $glmTargetPath)
+}
+
+$glmIntermediateDir = Join-Path $extDir "glm"
+$glmFinalDir = Join-Path $extDir "glm-$glmVersion"
+if (!(Test-Path $glmFinalDir) -And !(Test-Path $glmIntermediateDir)) {
+    Write-Output "[$step/$totalSteps] Unzipping GLM..."
+    Expand-Archive $glmTargetPath -DestinationPath $extDir
+}
+if (Test-Path $glmIntermediateDir) {
+    Rename-Item -path $glmIntermediateDir -newName $glmFinalDir
+}
+
+$step++
+# Set up Cereal
+$cerealTargetPath = Join-Path $extDir "cereal-$cerealVersion.zip"
+if (!(Test-Path $cerealTargetPath)) {
+    Write-Output "[$step/$totalSteps] Downloading Cereal..."
+    $cerealUrl = "https://github.com/USCiLab/cereal/archive/v" + $cerealVersion + ".zip"
+    $webClient.DownloadFile($cerealUrl, $cerealTargetPath)
+}
+
+$cerealFinalDir = Join-Path $extDir "cereal-$cerealVersion"
+if (!(Test-Path $cerealFinalDir)) {
+    Write-Output "[$step/$totalSteps] Unzipping Cereal..."
+    Expand-Archive $cerealTargetPath -DestinationPath $extDir
+}
+
+$step++
+# Set up stb_image
+$stbImageTargetPath = Join-Path $extDir "stb\stb\stb_image.h"
+if (!(Test-Path $stbImageTargetPath)) {
+    $stbImageTargetDir = Join-Path $extDir "stb\stb"
+    New-Item -ErrorAction Ignore -ItemType directory -Path $stbImageTargetDir | Out-Null
+
+    Write-Output "[$step/$totalSteps] Downloading stb_image..."
+    $stbImageUrl = "https://raw.githubusercontent.com/nothings/stb/master/stb_image.h"
+    $webClient.DownloadFile($stbImageUrl, $stbImageTargetPath)
+}
+
+$step++
+# Get NanoVG
+$nanoVgDir = Join-Path $extDir "nanovg"
+if (!(Test-Path $nanoVgDir)) {
+    Write-Output "[$step/$totalSteps] Cloning memononen/nanovg for build..."
+    git clone git@github.com:memononen/nanovg.git $nanoVgDir
+}
+
+$step++
+# Get premake5
+$premakeTargetPath = Join-Path $extDir "premake-$premakeVersion.zip"
+if (!(Test-Path $premakeTargetPath)) {
+    Write-Output "[$step/$totalSteps] Downloading Premake..."
+    $premakeUrl = "https://github.com/premake/premake-core/releases/download/v" + $premakeVersion + "/premake-" + $premakeVersion + "-windows.zip"
+    $webClient.DownloadFile($premakeUrl, $premakeTargetPath)
+}
+
+$premakeExePath = Join-Path $extDir "premake5.exe"
+if (!(Test-Path $premakeExePath)) {
+    Write-Output "[$step/$totalSteps] Unzipping premake..."
+    Expand-Archive $premakeTargetPath -DestinationPath $extDir
+}
+
+$step++
+# Build NanoVG
+$nanoVgBuildDir = Join-Path $nanoVgDir "build"
+$nanoVgFinalBuildDir = Join-Path $nanoVgDir "build-msvc"
+if (!(Test-Path $nanoVgBuildDir) -And !(Test-Path $nanoVgFinalBuildDir)) {
+    Write-Output "[$step/$totalSteps] Building NanoVG..."
+    try {
+        Write-Output "[$step/$totalSteps] Moving to $nanoVgDir."
+        Set-Location $nanoVgDir
+        
+        Write-Output "[$step/$totalSteps] Generating VS2019 solution..."
+        & "$premakeExePath" "vs2019"
+
+        if (Test-Path $nanoVgBuildDir) {
+            Rename-Item -path $nanoVgBuildDir -newName $nanoVgFinalBuildDir
+        }
+    } finally {
+        Set-Location $currentDir
+    }
+}
+
+$nanoVgSolution = Join-Path $nanoVgFinalBuildDir "nanovg.sln"
+$todoMessage = "TODO: Open solution " + $nanoVgSolution + " in Visual Studio and build project nanovg for Release/x64."
+
+$step++
+Write-Host "[$step/$totalSteps] Success."
+Write-Host $todoMessage
