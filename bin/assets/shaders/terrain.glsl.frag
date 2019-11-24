4954f9af2aa7179822c467ccd37888fdf7b3f23f
Krzysztof Antoniak <krzysztof@antoniak.in>
2019-11-24 18:13:52 +0100

    Update project to use OpenGL 4.3

--- bin/assets/shaders/terrain.glsl.frag
+++ bin/assets/shaders/terrain.glsl.frag
@@ -1,4 +1,4 @@
-#version 330 core
+#version 430 core
 
 uniform sampler2D groundTexture;
 uniform sampler2DArray roadTexture;
--- src/input/WindowHandler.cpp
+++ src/input/WindowHandler.cpp
@@ -32,7 +32,7 @@ bool WindowHandler::createMainWindow() {
   glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
 
   glfwWindowHint(GLFW_SAMPLES, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
